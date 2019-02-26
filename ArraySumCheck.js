69132050ba4c857eb9843982124150090ea4768d
Uzochukwu Onuegbu <uzochukwu.onuegbu@demzanalytics.com>
2019-02-26 15:38:07 +0100

    clean up code, and add comments

--- ArraySumCheck.js
+++ ArraySumCheck.js
@@ -1,3 +1,7 @@
+// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
 function numCheck(array1, total_num) {
 
   for (i=0; i<array1.length; i++) {
@@ -11,4 +15,6 @@ function numCheck(array1, total_num) {
   return false
 }
 
-console.log(numCheck([1, 2, 5, 3], 3))
+// You can run the code below and tweak the values to check
+
+// console.log(numCheck([1, 2, 5, 3], 10))
