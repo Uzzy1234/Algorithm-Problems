1753896a35c797c9f927f9cfefb93d7a299c7ce3
uzochukwuonuegbu <uzochukwu.onuegbu25@gmail.com>
2020-09-16 11:35:25 +0100

    Update primeNums.js

--- primeNums.js
+++ primeNums.js
@@ -1,14 +1,21 @@
 // Given a number, n, return the number of prime numbers from 1 - n
 
+function isPrime(num) {
+    for(var i = 2; i < num; i++)
+      if(num % i === 0) return false;
+    return num > 1;
+  }
+
 function primeNums(n) {
     result = []
     if (n == 1) return 1
     for (i = 2; i <= n; i++) {
-      if (i % 2 !== 0) {
-        result.push(i)
-      }
+        if (isPrime(i)) {
+            result.push(i)
+            console.log({ result })
+        }
     }
     return result.length
-  }
+}
   
 //   console.log(primeNums(1000))
